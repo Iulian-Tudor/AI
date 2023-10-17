170fca3c7f3810735dfdf54e1f670c86418180ac
Iulian-Tudor <itscutaru@gmail.com>
2023-10-17 17:54:41 +0300

    Fixed algorithm

--- Sudoku (Lab 2 si 3)/main.py
+++ Sudoku (Lab 2 si 3)/main.py
@@ -1,66 +1,74 @@
-
 import time
 
 # Funcția de inițializare
 def initialize(input_state):
-    state = [[0]*3 for _ in range(3)]
+    state = [[0] * 3 for _ in range(3)]
     empty_cell = None
-    for i in range(9):
-        state[i//3][i%3] = input_state[i]
-        if input_state[i] == 0:
-            empty_cell = (i//3, i%3)
+    for i in range(3):
+        for j in range(3):
+            state[i][j] = input_state[i * 3 + j]
+            if input_state[i * 3 + j] == 0:
+                empty_cell = (i, j)
     return state, empty_cell
-
 
 # Funcția de verificare a stării finale
 def is_final(state):
     for i in range(3):
         for j in range(3):
-            if state[i][j] != i*3 + j + 1 and state[i][j] != 0:
+            if state[i][j] != i * 3 + j + 1 and state[i][j] != 0:
                 return False
     return True
 
 # Funcțiile de tranziție
-def move(state, empty_cell, direction, history):
+def move(state, empty_cell, direction):
     x, y = empty_cell
-    if direction == 'up':
-        x -= 1
-    elif direction == 'down':
-        x += 1
-    elif direction == 'left':
-        y -= 1
-    elif direction == 'right':
-        y += 1
-    if 0 <= x < 3 and 0 <= y < 3:
-        if (x, y) not in history:
-            state[empty_cell[0]][empty_cell[1]], state[x][y] = state[x][y], state[empty_cell[0]][empty_cell[1]]
-            return state, (x, y), history + [(x, y)]
-    return None, None, history
+    if direction == 'up' and x > 0:
+        state[x][y], state[x - 1][y] = state[x - 1][y], state[x][y]
+        return state, (x - 1, y)
+    elif direction == 'down' and x < 2:
+        state[x][y], state[x + 1][y] = state[x + 1][y], state[x][y]
+        return state, (x + 1, y)
+    elif direction == 'left' and y > 0:
+        state[x][y], state[x][y - 1] = state[x][y - 1], state[x][y]
+        return state, (x, y - 1)
+    elif direction == 'right' and y < 2:
+        state[x][y], state[x][y + 1] = state[x][y + 1], state[x][y]
+        return state, (x, y + 1)
+    return None, empty_cell
 
 # Funcția de căutare IDDFS
-def iddfs(state, empty_cell, depth, history):
+def iddfs(state, empty_cell, depth):
     if is_final(state):
         return state
     if depth == 0:
         return None
     for direction in ['up', 'down', 'left', 'right']:
-        next_state, next_empty_cell, next_history = move(state, empty_cell, direction, history)
+        next_state, next_empty_cell = move(state, empty_cell, direction)
         if next_state is not None:
-            result = iddfs(next_state, next_empty_cell, depth - 1, next_history)
+            result = iddfs(next_state, next_empty_cell, depth - 1)
             if result is not None:
                 return result
     return None
 
-# Testarea funcțiilor
-state, empty_cell = initialize([2, 5, 3, 1, 0, 6, 4, 7, 8])
-print('Starea inițială:')
-print(state)
-print('Celula goală inițială:')
-print(empty_cell)
-end_time= time.time()
-solution = iddfs(state, empty_cell, 50, [empty_cell])
-execution_time = time.time() - end_time
-print('Soluția:')
-print(solution)
-print('Timpul de execuție:')
-print(execution_time)
+def main():
+    input_state = [8, 6, 7, 2, 5, 4, 0, 3, 1]
+    state, empty_cell = initialize(input_state)
+    print('Starea inițială:')
+    for row in state:
+        print(row)
+    print('Celula goală inițială:')
+    print(empty_cell)
+    start_time = time.time()
+    solution = iddfs(state, empty_cell, 30)  # Limita la adancime sa nu ruleze la infinit fara sa-mi dau seama
+    execution_time = time.time() - start_time
+    print('Soluția:')
+    if solution is not None:
+        for row in solution:
+            print(row)
+    else:
+        print('Nu există soluție.')
+    print('Timpul de execuție:')
+    print(execution_time)
+
+
+main()
